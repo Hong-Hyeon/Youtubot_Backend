8f3f383e798205a6026e69654b2cbe6795f2ae78
showmanships@naver.com <showmanships@naver.com>
2023-07-10 22:01:01 +0900

    setting update

--- config/settings.py
+++ config/settings.py
@@ -31,10 +31,13 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = [
-    'localhost',
-    'backend.ddonline.online',
-]
+if DEBUG:
+    ALLOWED_HOSTS=[]
+else:
+    ALLOWED_HOSTS = [
+        'localhost',
+        'backend.ddonline.online',
+    ]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
