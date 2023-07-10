49c9436f81c26d46c632842eb9e6ba168606e430
showmanships@naver.com <showmanships@naver.com>
2023-07-10 19:39:30 +0900

    build.sh update

--- build.sh
+++ build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
-poetry install
-
+pip install --upgrade pip
+pip install poetry==1.2.0
+rm poetry.lock
+poetry lock
+python -m poetry install
+pip install --force-reinstall -U setuptools
 python manage.py collectstatic --no-input
 python manage.py migrate
